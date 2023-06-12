6748b1a09678cdeb61df8a09f9ae23572ee63481
df <47768371+df-1945@users.noreply.github.com>
2023-06-12 12:38:42 +0700

    jker

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def index(data: DataRequest):
 async def main(headers, keyword, pages):
     product_soup = []
     async with async_playwright() as playwright:
-        browser = await playwright.chromium.launch()
+        browser = await playwright.firefox.launch(headless=True)
         context = await browser.new_context()
         
         loop = asyncio.get_event_loop()
